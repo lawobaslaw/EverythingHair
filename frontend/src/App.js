c9dd96265891aa2e6a2f3ade8b4c465540a9ce2d
lawobaslaw <lawobaslaw@gmail.com>
2021-05-31 14:54:37 -0500

    Ratings Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 
 import React from 'react';
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeView from './viewport/HomeView'
 const App = () => {
 
-  
+
   return (
-    <div>
- 
-      </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeView />
+        </Container>
+      </main>
+
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        &copy;2021 Everything Hair Limited, All Rights Reserved.
+                      </Col>
+                </Row>
+            </Container>
+
+        </footer>
+    )
+}
+
+export default Footer
